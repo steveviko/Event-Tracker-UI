e0bb72217677e5715ede4d757a8534eb0990ff4a
steveviko <stevenviko@gmail.com>
2018-09-05 08:39:22 +0300

    Add responsiveness on index page[Delivers #160221419]

--- app/signup.py
+++ app/signup.py
@@ -1,2 +1,10 @@
-class SignUp:
-    pass
+class SignUp():
+    def __init__(self):
+        self.user_details = dict()
+
+
+    def add(self,username,password):
+        self.user_details[username] = password
+
+    def get_password(self,username):
+        return self.user_details[username]
