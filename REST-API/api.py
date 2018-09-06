690fedb4793cdfd0d56dc4fe416b2aab1c5f3b33
steveviko <stevenviko@gmail.com>
2018-09-06 17:26:02 +0300

    Modified api routes[Delivers #160318734]

--- REST-API/api.py
+++ REST-API/api.py
@@ -1,20 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,json
 from users import User
 
 
 app = Flask(__name__)
 
+users = User()
+
 @app.route("/api/get-users", methods=['GET'])
-def get_users():
-    return "ALL users"
+def get_all_users():
+    return users.get_all_users()
 
 @app.route("/api/add", methods=['POST'])
 def add_users():
-    return "user id"
+    return users.post_user(username="",password="")
 
-@app.route("/api/delete", methods=['POST'])
+@app.route("/api/delete/<string:username>", methods=['DELETE'])
 def delete_user():
-    return "del id"
+    return users.delete_user()
 
 if __name__=='__main__':
     app.run(debug=True)
--- REST-API/users.py
+++ REST-API/users.py
@@ -1,2 +1,32 @@
+from flask import Flask, request, jsonify,json
+
 class User:
-    pass
+    def __init__(self):
+        self.users=[]
+   
+    def get_all_users(self):
+        return jsonify(self.users)
+
+
+    def delete_user(self,username):
+        found =False
+        for id, user in enumerate(self.users):
+            if user["username"] == username:
+                id = id
+                found = True
+                del self.users[id]
+            return "users: {0}".format(json.dumps(self.users))
+        return found
+
+
+    def post_user(self,username,password):
+        new_user = {}
+        username = new_user['username']
+        password = new_user['password']
+        self.users.append(new_user)
+        return json.dumps(new_user)
+        
+
+   
+       
+      
